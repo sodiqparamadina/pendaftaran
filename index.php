a735c7ed5faaf9ba9b7cd39c69a945f69b980a43
M Pawit <muhammad.pawit93@gmail.com>
2025-02-18 15:40:15 +0700

    variasi

--- index.php
+++ index.php
@@ -63,9 +63,11 @@
     </div>
     <div class="hero-section">
       <div class="hero-content">
-        <h1 class="hero-title">Portal Pendaftaran Mahasiswa Baru</h1>
+        <h1 class="hero-title">
+          Portal Pendaftaran Mahasiswa Baru
+        </h1>
         <p class="hero-subtitle">
-          Formulir pendaftaran mahasiswa baru di Universitas Paramadina.
+          <!-- Formulir pendaftaran mahasiswa baru di Universitas Paramadina. -->
         </p>
       </div>
     </div>
@@ -76,7 +78,9 @@
         method="POST"
       >
         <div>
-          <h1 class="text-gelombang" id="text-gelombang"></h1>
+          <h1 class="text-gelombangs" id="text-gelombangs">
+            Formulir Pendaftaran
+          </h1>
           <p class="text-semester" id="text-semester"></p>
         </div>
         <div class="form-radio">
