8f342aef81cd3a38117683093a07bd7e78581ab6
sodiqardianto <sodiqardianto@gmail.com>
2025-07-01 17:28:08 +0700

    update with time naming file css

--- index.php
+++ index.php
@@ -43,7 +43,7 @@
       href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
       rel="stylesheet"
     />
-    <link rel="stylesheet" href="/styles/style.css?v=20250701" />
+    <link rel="stylesheet" href="/styles/style.css?v=<?= filemtime('styles/style.css') ?>">
     <link rel="icon" href="images/logo.jpg" type="image/x-icon">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
   </head>
